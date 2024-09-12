ce153d066c0a3a935c2a0a4a527a8a6ee81a6cd1
Chandler Jacobs <jacobstechgroup@gmail.com>
2024-09-12 22:51:24 +0200

    First calculator commit

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+# firstcalc= int(input())
+# secondcalc= int(input())
+
+def calculation(firstcalc, operation, secondcalc):
+    if operation == '+':
+        return(firstcalc + secondcalc)
+    elif operation == '-':
+        return(firstcalc - secondcalc)
+    elif operation == '*':
+        return(firstcalc * secondcalc)
+    elif operation == '/':
+        return(firstcalc/ secondcalc)
+    else:
+        return("Incorrect input, please try again")
+    
+print(calculation("1", '-', 2))
