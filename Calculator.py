0990a0764c51c9629d4922104ca667f2844d80f2
Chandler Jacobs <jacobstechgroup@gmail.com>
2024-09-20 14:34:21 +0200

    Update

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,39 @@
-# firstcalc= int(input())
-# secondcalc= int(input())
+print("Hello, welcome to calculator!")
+a= int(input("Please enter your first number:"))
+b= int(input("Please enter your second number:"))
 
-def calculation(firstcalc, operation, secondcalc):
-    if operation == '+':
-        return(firstcalc + secondcalc)
-    elif operation == '-':
-        return(firstcalc - secondcalc)
-    elif operation == '*':
-        return(firstcalc * secondcalc)
-    elif operation == '/':
-        return(firstcalc/ secondcalc)
-    else:
-        return("Incorrect input, please try again")
+calc2 = input("Select an operator +, -, /, *")
+
+def add_ (a, b):
+        
+    add = a + b 
+    return add
+
+def sub_ (a, b):
+        
+    sub = a-b
+    return sub
+
+def div_ (a, b):
     
-print(calculation("1", '-', 2))
+    divided = a/b
+    return divided
+       
+def mult_ (a, b):
+    
+    divided=a*b
+    return divided
+
+if calc2 == "+":
+    print(f"The sum of the equation is: {add_(a, b)}" )
+elif calc2 == "-":
+    print(f"The difference of the equation is: {sub_(a, b)}" )
+elif calc2 == "/":
+    print(f"The dividend of the equation is: {div_(a, b)}" )
+elif calc2 == "*":
+    print(f"The product of the equation is: {mult_(a, b)}" )
+
+
+print("Thank you for using calculator!")
+
+
